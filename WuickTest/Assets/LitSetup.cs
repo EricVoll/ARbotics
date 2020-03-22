5d8c1e69f6854d982c413db917cd60955670100a
Eric Vollenweider <eric.vollenweider@gmail.com>
2020-03-22 17:45:34 +0100

    RosSharp running and comm. with ubuntu

---
+++ WuickTest/Assets/LitSetup.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LitSetup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Libarary Logger values set!");
+        Assets.RosSharp.Scripts.Logger.setLogDelegate((o) => Debug.Log(o));
+        Assets.RosSharp.Scripts.Logger.setErrorDelegate((o) => Debug.LogError(o));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
