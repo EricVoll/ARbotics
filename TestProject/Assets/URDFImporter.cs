18926040e32d34ab5f09025221bdd829658a4120
Eric Vollenweider <eric.vollenweider@gmail.com>
2020-03-22 09:13:04 +0100

    First attempts to connect Unity to ROS via ROS#

---
+++ TestProject/Assets/URDFImporter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class URDFImporter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //private TransferFromRosHandler transferHandler;
+    void Start()
+    {
+
+        //Thread rosSocketConnectThread = new Thread(() => transferHandler.TransferUrdf(protocolType, address, timeout, assetPath, urdfParameter, serializerType));
+        //rosSocketConnectThread.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
