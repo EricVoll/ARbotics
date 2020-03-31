641ebde487989e6792921e3b79abe72776ed0cca
Jonas <jonfrey@student.ethz.ch>
2020-03-31 17:25:40 +0200

    untested compose file and ros1_gazebo container

--- Docker/ros1_moveit/moveit_install.sh
+++ Docker/ros1_moveit/moveit_install.sh
@@ -27,6 +27,6 @@ echo 'source /home/catkin_ws/devel/setup.bash' >> ~/.bashrc
 
 # sudo docker build -t ros1_ros-sharp:1.0 .
 #sudo docker run -it --network=host ros1_ros-sharp:1.0
-#rostopic pub -r 10  euclidean_goal_pose geometry_msgs/Pose {position:  {x: 0.6, y: 0.6, z: 0.40}, orientation: {x: 0.0,y: 0.0,z: 0.0, w: 1.2}}'
+#rostopic pub -r 10  euclidean_goal_pose geometry_msgs/Pose {position:  {x: 0.6, y: 0.6, z: 0.40}, orientation: {x: 0.0,y: 0.0,z: 0.0, w: 1.2}}
 
 #roslaunch ur_description ur5_upload.launch
