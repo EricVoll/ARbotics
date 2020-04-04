bbe7aac637fa0833f27e2f8755446b6756b77226
Eric Vollenweider <eric.vollenweider@gmail.com>
2020-04-04 20:37:51 +0200

    Base64Image Publisher & Subscriber

---
+++ HololensTests/Assets/Base64ImagePublisher.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RosSharp.RosBridgeClient
+{
+    public class Base64ImagePublisher : Publisher<Messages.Standard.String>
+    {
+        public Camera ImageCamera;
+        public string FrameId = "Camera";
+        public int resolutionWidth = 640;
+        public int resolutionHeight = 480;
+        [Range(0, 100)]
+        public int qualityLevel = 50;
+
+        private Messages.Standard.String message;
+        private Texture2D texture2D;
+        private Rect rect;
+
+        protected override void Start()
+        {
+            base.Start();
+            InitializeGameObject();
+            InitializeMessage();
+            Camera.onPostRender += UpdateImage;
+        }
+
+        private void UpdateImage(Camera _camera)
+        {
+            if (texture2D != null && _camera == this.ImageCamera)
+                UpdateMessage();
+        }
+
+        private void InitializeGameObject()
+        {
+            texture2D = new Texture2D(resolutionWidth, resolutionHeight, TextureFormat.RGB24, false);
+            rect = new Rect(0, 0, resolutionWidth, resolutionHeight);
+            ImageCamera.targetTexture = new RenderTexture(resolutionWidth, resolutionHeight, 24);
+        }
+
+        private void InitializeMessage()
+        {
+            message = new Messages.Standard.String(){
+                data = "",
+            };
+        }
+
+        private void UpdateMessage()
+        {
+            texture2D.ReadPixels(rect, 0, 0);
+            message.data = System.Convert.ToBase64String(texture2D.EncodeToJPG(qualityLevel));
+            Publish(message);
+        }
+
+    }
+}
---
+++ HololensTests/Assets/Base64ImageSubscriber.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RosSharp.RosBridgeClient
+{
+    [RequireComponent(typeof(RosConnector))]
+    public class Base64ImageSubscriber : Subscriber<Messages.Standard.String>
+    {
+        public MeshRenderer meshRenderer;
+
+        private Texture2D texture2D;
+        private byte[] imageData;
+        private bool isMessageReceived;
+
+        protected override void Start()
+        {
+            base.Start();
+            texture2D = new Texture2D(1, 1);
+            meshRenderer.material = new Material(Shader.Find("Standard"));
+        }
+        private void Update()
+        {
+            if (isMessageReceived)
+                ProcessMessage();
+        }
+
+        protected override void ReceiveMessage(RosSharp.RosBridgeClient.Messages.Standard.String msg)
+        {
+            imageData = Convert.FromBase64String(msg.data);
+            isMessageReceived = true;
+        }
+
+        private void ProcessMessage()
+        {
+            texture2D.LoadImage(imageData);
+            texture2D.Apply();
+            meshRenderer.material.SetTexture("_MainTex", texture2D);
+            isMessageReceived = false;
+        }
+
+    }
+}
