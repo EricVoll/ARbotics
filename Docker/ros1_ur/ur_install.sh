02547c9580f8377b1285d061706b93cde7086302
Jonas <jonfrey@student.ethz.ch>
2020-04-04 23:13:19 +0200

    Tested
    Docker-compose up works now. Rosbridge UR5 Gazebo Moveit

--- Docker/ros1_ur/ur_install.sh
+++ Docker/ros1_ur/ur_install.sh
@@ -19,7 +19,13 @@ sudo apt-get install -y ros-melodic-moveit
 echo 'source /opt/ros/melodic/setup.bash' >> ~/.bashrc 
 echo 'source /home/catkin_ws/devel/setup.bash' >> ~/.bashrc 
 
-
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+curl -sSL 'http://keyserver.ubuntu.com/pks/lookup?op=get&search=0xC1CF6E31E6BADE8868B172B4F42ED6FBAB17C654' | sudo apt-key add -
+sudo apt update
+sudo apt install -y ros-melodic-desktop-full
+echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
 # #soruce workspace
 # source /home/catkin_ws/devel/setup.bash
 # sudo apt-get install ros-melodic-moveit
