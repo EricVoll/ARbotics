6e1590cbe3e383d19dbec366d8dc9e85963de437
JonasFrey96 <frey.breitenbrunn@gmx.de>
2020-04-25 13:44:58 +0200

    Prepare AR_Manager container

--- AR_Manager/src/ros_nodes/server_state_publisher.py
+++ AR_Manager/src/ros_nodes/server_state_publisher.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 import rospy 
-from rospy_message_converter import message_converter
-from std_msgs.msg import String
-msg = String()
-import std_msgs.msg
-msg = std_msgs.msg.String()
-import rosparam
 
+#from rospy_message_converter import message_converter
+#ROS 1 specific approach
+#This can be used to map between costum ROS MSG and dict !
 #string seralization https://github.com/uos/rospy_message_converter
+
+from std_msgs.msg import String
+import std_msgs.msg
+import rosparam
+import json
+
 
 def shutdown_hook():
   print ("Close Server State Publisher")
@@ -24,58 +27,32 @@ class ARServerStatePublisher():
 
 	def publish(self, data):
 
-
-
-
-		print("not called anymore")
-		
 		if not isinstance(data, dict):
-			print("ERROR2")
 			raise TypeError("data is not a <dict>")
 		if not ( 'data' in data.keys() ):
-			print("ERROR1")
 			raise ValueError("dict must contain key <data> entry")
-
 		
-		print("HERE WE G")
-		print(data)
-		data = {'data': "string"}
-		msg = message_converter.convert_dictionary_to_ros_message('std_msgs/String', data)
-		val = self.pub.publish( msg )
-		print("MSG MSG:", msg.data)
-		# except rospy.ROSInterruptException:
-		# 	print("ERROR3")
-		# 	print("ROS interrupt exeption during pub. data")	
-	
-		#self.rate.sleep()
-	
+		#pass by string #serialization could also be done by protobuff
+		json_mylist = json.dumps(data, separators=(',', ':'))	
+		ros_msg = String(data=json_mylist)
+
+		self.pub.publish( ros_msg )
 
 
 
 if __name__ == '__main__':
 
-	#parameter server is working easily
-
-
+	#parameter server examples
 	# rosparam.set_param('global_example',"example")
 	# rosparam.set_param('list_of_floats', "[1., 2., 3., 4.]") #use yaml
 	# rosparam.set_param('gains', "{'p': 1, 'i': 2, 'd': 3}")
 	# rospy.get_param('gains/p') #should return 1
 	# param_name = rospy.search_param('global_example')
 	# v = rospy.get_param(param_name)
-		# while not rospy.is_shutdown():
-		# #rospy.loginfo(hello_str)
-		
-		# dictionary = { 'data': {'key': [1, 2, 3], 'robot': "value"}}
-		
-		# print(message.data)
-
-		
-		# rate.sleep()
-
-
 	# if rospy.has_param('gains/i'):
 	# 	rospy.delete_param('gains/i')
+
+
 	AsSSP = ARServerStatePublisher()
 	dictionary = { 'data': {'key': [1, 2, 3], 'robot': "value"}}
 	AsSSP.publish(data=dictionary)
