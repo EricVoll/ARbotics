d7fd70dc6068295fc7890bc5fb46753831623d5d
JonasFrey96 <frey.breitenbrunn@gmx.de>
2020-05-01 15:30:04 +0200

    fix:sigterm

--- AR_Manager/src/main.py
+++ AR_Manager/src/main.py
@@ -38,22 +38,7 @@ if __name__ == '__main__':
 				raise KeyboardInterrupt("STRG C")
 
 		signal.signal(signal.SIGINT, signal_handler)
-		# signal.signal(signal.SIGHUP, signal_handler)
-		# signal.signal(signal.SIGINT, signal_handler)
-		# signal.signal(signal.SIGQUIT, signal_handler)
-		# signal.signal(signal.SIGILL, signal_handler)
-		# signal.signal(signal.SIGTRAP, signal_handler)
-		# signal.signal(signal.SIGABRT, signal_handler)
-		# signal.signal(signal.SIGBUS, signal_handler)
-		# signal.signal(signal.SIGFPE, signal_handler)
-		signal.signal(signal.SIGKILL, signal_handler)
-		# signal.signal(signal.SIGUSR1, signal_handler)
-		# signal.signal(signal.SIGSEGV, signal_handler)
-		# signal.signal(signal.SIGUSR2, signal_handler)
-		# signal.signal(signal.SIGPIPE, signal_handler)
-		# signal.signal(signal.SIGALRM, signal_handler)
-		signal.signal(signal.SIGTERM, signal_handler)	
-
+		signal.signal(signal.SIGTERM, signal_handler)
 		api.add_resource(ResInstances,'/Instances',
 			resource_class_kwargs={'server': s})
 		api.add_resource(ResInstance,'/Instances/<int:inst_id>',
