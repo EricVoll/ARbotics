8b8181d88550946d10da4b2336ef0e228412d6b5
Eric Vollenweider <eric.vollenweider@gmail.com>
2020-05-16 10:59:00 +0200

    Add a TargetObject for IK w/ Ros# Publishing

---
+++ ARManager/Assets/ARCommandManagerScripts/EndEffectorTarget.cs
@@ -0,0 +1,26 @@
+﻿using RosSharp.RosBridgeClient;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndEffectorTarget : MonoBehaviour
+{
+    public RosConnector RosConnector;
+    private EndEffectorTargetPublisher publisher;
+    public string Topic;
+    public bool Publish;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        publisher = RosConnector.gameObject.AddComponent<EndEffectorTargetPublisher>();
+        publisher.Topic = Topic;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Publish)
+            publisher.Publish(transform.position, transform.rotation);
+    }
+}
