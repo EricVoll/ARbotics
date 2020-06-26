9391f1941ebb392e13ea1393b08876591b5f9100
Raffael Theiler <rptheiler@gmail.com>
2020-06-26 12:44:22 +0200

    remove unnecessary copy

--- Tooltips - Copy/Assets/Turcan/Scripts/ISensorDataPublisher.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface ISensorDataPublisher 
-{
-    Action<Vector3[]> ExtractPointCallBack { get; set; }
-}
